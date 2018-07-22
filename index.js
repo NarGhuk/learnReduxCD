63c8edae26732ca8efc55d31390eea069779f177
NarGhuk <nar.ghuk777@gmail.com>
2018-07-22 12:20:08 +0400

    create class Store

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 function updateState(state, action) {
   if (action.type === "INCREMENT") {
     return state + action.amount;
@@ -13,12 +11,13 @@ class Store{
   constructor(updateState,state){
     this._state = state;
     this._updateState = updateState;
+    this.callbacks = [];
   }
   get state(){
     return this._state
   }
   update(action){
-    this._state= this._updateState(this.state,action);
+    this._state = this._updateState(this._state, action);
   }
 }
 const store = new Store(updateState,0);
@@ -27,9 +26,10 @@ const incrementAtion = {type: 'INCREMENT', amount: 5};
 const decrementAtion = {type: 'DECREMENT', amount: 3};
 
 store.update(incrementAtion);
-
 console.log(store.state);
+
 store.update(decrementAtion);
 console.log(store.state);
-store.update= updateState({});
+
+store.update({});
 console.log(store.state);
